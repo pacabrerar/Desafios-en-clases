2e7b634ff638c22037990ff15f609acf46a930f2
pacabrerar <49323610+pacabrerar@users.noreply.github.com>
2019-06-20 16:01:01 -0400

    Ejercicio 17

---
+++ Ejercicio 17/ejercicio.js
@@ -0,0 +1,14 @@
+var nombre = prompt('Ingrese su nombre:');
+console.log('Hola ' + nombre + '!');
+
+var numero = prompt('Ingrese un numero:');
+var numero2 = 10;
+console.log(parseInt(numero)+numero2);
+
+var numero = prompt('Ingrese un numero:');
+var numero2 = 10;
+console.log(parseInt(numero)-numero2);
+
+var numero_1 = prompt('Ingrese un numero 1:');
+var numero_2 = prompt('Ingrese un numero 2:');
+console.log(parseInt(numero_1)+parseInt(numero_2));
---
+++ Ejercicio 17/intro.js
@@ -0,0 +1,94 @@
+//Variable!!!
+// var nombredelavariable;
+var primerNombre;
+
+primerNombre = 'Juan';
+
+primerNombre = 'Pedro';
+
+// pueden comenzar abc... _  $
+
+var $precio = 25;
+var _total;
+var texto = 'mi primer texto';
+
+// nombre No validos
+// var 1texto;
+// var &texto;
+
+nombrarUnaVariableMultiple
+nombrar_una_variable_multiple
+
+// Que va dentro de una variable?
+// Variables Booleanas
+
+var booleana1 = true;
+var booleana2 = false;
+
+// Variables Numericas
+
+var intNumber = 3; // -10 4 5 13984
+var floatNumber = 0.4129;
+var negativeFloatNumber = -8.1476;
+
+// Variables de cadenas de textos "STRINGS"
+
+var primerNombre = 'Diego';
+var texto = 'don\'t\\';
+
+// Contenido nulo o indefinido (null or undefined)
+
+var ejemplo1; // esta variable es undefined
+var ejemplo2 = null; //variable con valor nulo
+
+// Arreglos (Matrices "Mixtas")
+
+var arreglo = [1, null, 'Juan', 723.983];
+
+arreglo[0] // retorna el primer elemento
+arreglo[2] // retorna el tercer elemento
+
+// Objetos
+var usuario = {
+  primerNombre : 'Diego',
+  edad : 34,
+  correo : 'hola@ejemplo.com'
+}
+
+usuario.primerNombre; //retorna 'Diego'
+usuario.edad = 43;   //asignar un valor
+usuario['correo'];   //devuelve 'hola@ejemplo.com'
+
+// Operaciones Aritméticas
+
+// Suma
+var total = 'String' + 2; // concatenar
+var total = 2 + 2;
+
+true; // equivale a un 1
+false; // equivale a un 0
+
+// Resta
+8 - 10;
+
+5 - '2'; // si puede transformar el texto en numero, de poder hacerlo la resta se realiza
+
+// Multiplicación
+
+3*5;
+5*total   // 20
+
+
+// División
+
+5/2;
+total/7;
+
+// Modulo %, devuelve el resto de la división
+
+total%7
+
+// Incremental y Decremental
+
+i++;
+i--;
